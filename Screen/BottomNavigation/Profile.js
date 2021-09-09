9da9ab19177b6e6f4c0c39ff2c0b889498f75452
sahilwins <sahilshwins@gmail.com>
2021-09-09 18:59:19 +0530

    hhh

--- Screen/BottomNavigation/Profile.js
+++ Screen/BottomNavigation/Profile.js
@@ -8,9 +8,13 @@ import {
   Button,
 } from "react-native";
 import { min } from "react-native-reanimated";
+import { useEffect } from "react/cjs/react.development";
 
 const Profile = () => {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("test use effect", count);
+  });
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Button title="Dec" onPress={() => setCount(Math.max(count - 1, 0))} />
@@ -19,7 +23,7 @@ const Profile = () => {
           <Text>{count}</Text>
         </View>
       </ScrollView>
-      <Button title="inc" onPress={() => setCount(Math.min(count +1))} />
+      <Button title="inc" onPress={() => setCount(Math.min(count + 1))} />
     </SafeAreaView>
   );
 };
@@ -27,3 +31,36 @@ const Profile = () => {
 export default Profile;
 
 const styles = StyleSheet.create({});
+
+// import React, { useState } from "react";
+// import { View, Switch, StyleSheet, Text } from "react-native";
+
+// const Profile = () => {
+//   const [isEnabled, setIsEnabled] = useState(false);
+//   const toggleSwitch = () => setIsEnabled(!isEnabled);
+
+//   return (
+//     <View style={[styles.container, {backgroundColor:isEnabled ? "#f5dd4b" : "#f4f3f4"}]}>
+//       <Text>PRESS</Text>
+//       <Switch
+
+//         trackColor={{ false: "#767577", true: "#81b0ff" }}
+//         thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+//         ios_backgroundColor="#3e3e3e"
+//         onValueChange={toggleSwitch}
+//         value={isEnabled}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+
+//   },
+// });
+
+// export default Profile;
